c1ac857460e6cfcbca7a26cd56b18f224e8d5547
OTDF Studios Official <168537533+OTDFStudiosOFFICIAL@users.noreply.github.com>
2024-08-02 19:13:46 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,22 +2,33 @@
 #include "Settings.h"
 #include "Title.h"
 #include "Game.h"
+#include "LanguageManager.h"
+#include "Setup.h"
+#include "OnlineMode.h"
+#include "SpriteRender.h"
+#include "SplashScreen.h"
+#include "ServerManager.h"
+#include "RequestFeedback.h"
 
 int main() {
     Settings settings;
-    settings.load();  // Load settings (e.g., fullscreen, borderless)
+    settings.load();
 
     sf::VideoMode videoMode(1366, 768);
-    sf::Uint32 style = settings.fullscreen ? sf::Style::Fullscreen : 
+    sf::Uint32 style = settings.fullscreen ? sf::Style::Fullscreen :
                        (settings.borderless ? sf::Style::None : sf::Style::Default);
 
     sf::RenderWindow window(videoMode, "eMusiMath", style);
     window.setVerticalSyncEnabled(true);
+
+    SplashScreen splash(window);
+    splash.show();
 
     Title titleScreen(window);
     if (titleScreen.show()) {
         Game game(window);
         game.start();
     }
+
     return 0;
 }
